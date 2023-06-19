a53277ed09fcc3b2cf7e3956696c0b2a073d1d02
Lucerna00 <pjs3859811@naver.com>
2023-06-20 07:35:43 +0900

    add LounchScreen and icon

--- DAIL_E/DAIL_E/DAIL_EApp.swift
+++ DAIL_E/DAIL_E/DAIL_EApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DAIL_EApp: App {
     var body: some Scene {
         WindowGroup {
-            Home()
+            LounchScreenView()
         }
     }
 }
---
+++ DAIL_E/DAIL_E/User Interface/LounchScreen.swift
@@ -0,0 +1,47 @@
+//
+//  LounchScreen.swift
+//  chatgpt_v2
+//
+//  Created by Sherzod Makhmudov on 2023/04/25.
+//
+
+import SwiftUI
+
+struct LounchScreenView: View {
+    @State var isActive : Bool = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    var body: some View {
+            if isActive {
+                Home()
+            } else {
+                VStack {
+                    VStack {
+                        Image("Lounch")
+                    }
+                    .scaleEffect(size)
+                    .opacity(opacity)
+                    .onAppear {
+                        withAnimation(.easeIn(duration: 1.2)) {
+                            self.size = 0.9
+                            self.opacity = 1.00
+                        }
+                    }
+                }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                        withAnimation {
+                            self.isActive = true
+                        }
+                    }
+                }
+            }
+        }
+}
+
+struct LounchScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LounchScreenView()
+    }
+}
